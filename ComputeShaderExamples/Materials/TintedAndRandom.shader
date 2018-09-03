47fb2a90a68aae60d6495f1edf9d4c9150823398
Luka Kostic <3luka1@gmail.com>
2018-09-03 18:07:00 +0200

    stuff

---
+++ ComputeShaderExamples/Materials/TintedAndRandom.shader
@@ -0,0 +1,91 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "XParticle/1 - FreeParticle"
+{
+	Properties
+	{
+		_Tint ("Tint", Color) = (0, 0, 0.5, 0.3)
+		_Randomness ("Random", Range(0.0, 1.0)) = 0.5
+	}
+
+	SubShader 
+	{
+		Pass 
+		{
+			Blend SrcAlpha one
+
+			CGPROGRAM
+			#pragma target 5.0
+			
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			
+			// Particle's data
+			struct Particle
+			{
+				float3 position;
+				float3 velocity;
+			};
+			
+			// Pixel shader input
+			struct PS_INPUT
+			{
+				float4 position : SV_POSITION;
+				float4 color : COLOR;
+			};
+			
+			// Particle's data, shared with the compute shader
+			StructuredBuffer<Particle> particleBuffer;
+			
+			// Properties variables
+			uniform float4 _Tint;
+			uniform float _Randomness;
+			
+			// Vertex shader
+			PS_INPUT vert(uint vertex_id : SV_VertexID, uint instance_id : SV_InstanceID)
+			{
+				PS_INPUT o = (PS_INPUT)0;
+
+				// Color
+				float speed = length(particleBuffer[instance_id].velocity);
+
+				//float lerpValue = clamp(speed / _HighSpeedValue, 0.0f, 1.0f);
+				//float lerpValue = fmod(speed, 1.01f);
+				//o.color = lerp(_ColorLow, _ColorHigh, lerpValue);
+
+				float4 c = _Tint;
+
+				c.r *= 1.0 - _Randomness;
+				c.g *= 1.0 - _Randomness;
+				c.b *= 1.0 - _Randomness;
+
+				c.r += fmod(abs(particleBuffer[instance_id].velocity.x), _Randomness);
+				c.g += fmod(abs(particleBuffer[instance_id].velocity.y), _Randomness);
+				c.b += fmod(abs(particleBuffer[instance_id].velocity.z), _Randomness);
+
+				c.r = min(c.r, 1.0);
+				c.g = min(c.g, 1.0);
+				c.b = min(c.b, 1.0);
+				c.a = 1;
+				o.color = c;
+
+				// Position
+				o.position = UnityObjectToClipPos(float4(particleBuffer[instance_id].position, 1.0f));
+
+				return o;
+			}
+
+			// Pixel shader
+			float4 frag(PS_INPUT i) : COLOR
+			{
+				return i.color;
+			}
+			
+			ENDCG
+		}
+	}
+
+	Fallback Off
+}
---
+++ ComputeShaderExamples/Particles/Fountain/Fountain.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+public class Fountain : MonoBehaviour {
+
+    /// Particle data structure used by the shader and the compute shader. Must be same in both.
+    private struct Particle
+    {
+        public Vector3 position;
+        public Vector3 velocity;
+        public Vector3 startingVelocity;
+        public float lifetime;
+    }
+
+    /// Number of Particle created in the system.
+    public int particleCount = 10000;
+
+    /// Material used to draw the Particle on screen.
+    public Material material;
+
+    /// Compute shader used to update the Particles.
+    public ComputeShader computeShader;
+
+    /// Size in octet of the Particle struct. To be changed if particle struct changed.
+    private const int SIZE_PARTICLE = 24;
+
+    /// Id of the kernel used.
+    private int mComputeShaderKernelID;
+
+    /// Buffer holding the Particles.
+    ComputeBuffer particleBuffer;
+
+    /// Number of particle per warp.
+    private const int WARP_SIZE = 256;
+
+    /// Number of warp needed.
+    private int mWarpCount;
+
+    void Start()
+    {
+        // Calculate the number of warp needed to handle all the particles
+        if (particleCount <= 0)
+            particleCount = 1;
+        mWarpCount = Mathf.CeilToInt((float)particleCount / WARP_SIZE);
+
+        // Initialize the Particle at the start
+        Particle[] particleArray = new Particle[particleCount];
+        for (int i = 0; i < particleCount; ++i)
+        {
+            particleArray[i].position.x = 0;
+            particleArray[i].position.y = 0;
+            particleArray[i].position.z =0;
+
+            particleArray[i].velocity.x = Random.Range(-2f, 2f);
+            particleArray[i].velocity.y = 20f;
+            particleArray[i].velocity.z = Random.Range(-2f, 2f);
+
+            particleArray[i].startingVelocity = particleArray[i].velocity;
+
+            particleArray[i].lifetime = Random.Range(0f, 5f);
+        }
+
+        // Create the ComputeBuffer holding the Particles
+        particleBuffer = new ComputeBuffer(particleCount, SIZE_PARTICLE);
+        particleBuffer.SetData(particleArray);
+
+        // Find the id of the kernel
+        mComputeShaderKernelID = computeShader.FindKernel("CSMain");
+
+        // Bind the ComputeBuffer to the shader and the compute shader
+        computeShader.SetBuffer(mComputeShaderKernelID, "particleBuffer", particleBuffer);
+        material.SetBuffer("particleBuffer", particleBuffer);
+    }
+
+    void OnDestroy()
+    {
+        if (particleBuffer != null)
+            particleBuffer.Release();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 m = Input.mousePosition;
+        m.z = -transform.position.z;
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(m);
+        float[] mousePosition2D = { mousePosition.x, mousePosition.y };
+
+        // Send datas to the compute shader
+        computeShader.SetFloat("deltaTime", Time.deltaTime);
+        computeShader.SetFloats("mousePosition", mousePosition2D);
+
+        // Update the Particles
+        computeShader.Dispatch(mComputeShaderKernelID, mWarpCount, 1, 1);
+    }
+
+    void OnRenderObject()
+    {
+        material.SetPass(0);
+        Graphics.DrawProcedural(MeshTopology.Points, 1, particleCount);
+    }
+}
